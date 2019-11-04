f32c0db1747783164d5c4dcab3d54e1bcb523110
Mehul Gohil <mehul.gohil0810@gmail.com>
2019-11-04 09:07:08 +0530

    fix: define give-square.js in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ const config = {
 		give: [ './assets/src/css/frontend/give-frontend.scss', './assets/src/js/frontend/give.js' ],
 		'give-stripe': [ './assets/src/js/frontend/give-stripe.js' ],
 		'give-stripe-checkout': [ './assets/src/js/frontend/give-stripe-checkout.js' ],
+		'give-square': [ './assets/src/js/frontend/give-square.js' ],
 		admin: [ './assets/src/css/admin/give-admin.scss', './assets/src/js/admin/admin.js' ],
 		'babel-polyfill': '@babel/polyfill',
 		gutenberg: './blocks/load.js',
